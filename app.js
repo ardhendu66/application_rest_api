68d215d6521a61bd3d9440f0b34fcda1e087312f
ardhendu66 <140969694+ardhendu66@users.noreply.github.com>
2023-08-17 04:22:33 +0530

    app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.get('/', function(req,res){
     res.send(`<h1>In Home page right now</h1>`)
 })
-app.use('/comp', require('./src/router/route'))
+app.use('/comp', require('./route'))
 
 app.listen(port, function(){
     console.log(`Server running on PORT: http://localhost:${port}`)
